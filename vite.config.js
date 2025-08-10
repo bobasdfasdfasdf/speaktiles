d8be6b0dcf2d807efe5d51fd6697e7f2c790fc7e
Aaditya Chakravarthy <aaditya.chakravarthy@gmail.com>
2025-08-10 17:30:06 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    allowedHosts: ['all'], // allow all hosts
+    host: true, // listen on all addresses
+    port: 5173
+  }
 })
